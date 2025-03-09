7c203c6e968a2075a96bbf105fd861d7755a8a58
Jack <132464970+jackhfernandez@users.noreply.github.com>
2025-03-08 23:51:35 -0500

    Funciones...

--- s01/src/com/example/Funciones.java
+++ s01/src/com/example/Funciones.java
@@ -2,24 +2,30 @@ package com.example;
 
 public class Funciones {
   public static void main(String[] args) {
-    // Opcion 1
+    // Opcion 1: // funcion sin parametro y sin tipo de retorno
     showMenu();
 
-    // Opcion 2
-    getMenu();
+    // Opcion 2: // funcion sin parametro y con tipo de retorno
+    System.out.println(getMenu());
+
+    double price = getPrice();
+    System.out.println(price);
   }
 
-  // funcion sin parametro y sin tipo de retorno
+  static double getPrice() {
+    return 100.99;
+  }
+
   static void showMenu(){
     System.out.println("1. Shutdown computer");
     System.out.println("2. Restart computer");
     System.out.println("3. Log off");
     System.out.println("4. Manual shutdown settings");
     System.out.println("5. Abort shutdown");
-    System.out.println("6. Exit");
+    System.out.println("6. Exit\n");
   }
 
-  static void getMenu(){
-    
+  static String getMenu(){
+    return "Bienvenidos al menu: \n1. Shutdown com...";
   }
 }
