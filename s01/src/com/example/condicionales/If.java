a55693f9624fd6e653637ec1b28d864bc4845891
Jack <132464970+jackhfernandez@users.noreply.github.com>
2025-03-20 00:00:33 -0500

    Agregando if...

--- s01/src/com/example/condicionales/If.java
+++ s01/src/com/example/condicionales/If.java
@@ -2,6 +2,9 @@ package com.example.condicionales;
 
 public class If {
   public static void main(String[] args) {
-
+    int edad = 17;
+    if (edad >= 18) {
+      System.out.println("Es mayor de edad");
+    }
   }
 }
