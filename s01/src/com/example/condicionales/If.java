e79486cc55e9acf1f37652cd2edd560b75f07c8b
Jack <132464970+jackhfernandez@users.noreply.github.com>
2025-03-24 23:49:40 -0500

    Agregando if else...

--- s01/src/com/example/condicionales/If.java
+++ s01/src/com/example/condicionales/If.java
@@ -3,8 +3,12 @@ package com.example.condicionales;
 public class If {
   public static void main(String[] args) {
     int edad = 17;
-    if (edad >= 18) {
+
+    boolean esMayor = edad >= 18;
+
+    if (esMayor) {
       System.out.println("Es mayor de edad");
     }
+    System.out.println("Es menor de edad");
   }
 }
