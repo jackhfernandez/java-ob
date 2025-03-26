3e18f0eaa1180c18807ff2cdf519fafe8f325870
Jack <132464970+jackhfernandez@users.noreply.github.com>
2025-03-25 23:41:39 -0500

    Agregando if else if, switch...

--- s01/src/com/example/condicionales/IfElseIf.java
+++ s01/src/com/example/condicionales/IfElseIf.java
@@ -1,4 +1,37 @@
 package com.example.condicionales;
 
 public class IfElseIf {
+  public static void main(String[] args) {
+
+    String dia = "uhkn";
+
+    // Ejemplos de comparar
+    boolean resultadoNume = 5 == 5;
+    boolean resultado = dia.equals("Martes");
+
+    // if else if
+    if (dia.equals("Lunes")) {
+      System.out.println("Animo con la semana champions");
+
+    } else if (dia.equals("Martes")) {
+      System.out.println("Martes con M de Me besas");
+
+    } else if (dia.equals("Miercoles")) {
+      System.out.println("A la m todo");
+
+    } else if (dia.equals("Jueves")) {
+      System.out.println("Ya es juergues");
+
+    } else if (dia.equals("Viernes")) {
+      System.out.println("Nos fuimos");
+
+    } else if (dia.equals("Sabado")) {
+      System.out.println("Nos morimos");
+
+    } else if (dia.equals("Domingo")) {
+      System.out.println("Depresion");
+    } else {
+      System.out.println("No es un dia valido!");
+    }
+  }
 }
--- s01/src/com/example/condicionales/Switch.java
+++ s01/src/com/example/condicionales/Switch.java
@@ -3,5 +3,33 @@ package com.example.condicionales;
 public class Switch {
   public static void main(String[] args) {
 
+    String dia = "ahhgas;";
+
+    switch (dia) {
+      case "Lunes":
+        System.out.println("Es lunes");
+        break;
+      case "Martes":
+        System.out.println("Es martes");
+        break;
+      case "Miercoles":
+        System.out.println("Es miercoles");
+        break;
+      case "Jueves":
+        System.out.println("Es jueves");
+        break;
+      case "Viernes":
+        System.out.println("Es viernes");
+        break;
+      case "Sabado":
+        System.out.println("Es sabado");
+        break;
+      case "Domingo":
+        System.out.println("Es domingo");
+        break;
+      default:
+        System.out.println("Opcion invalida");
+        break;
+    }
   }
 }
