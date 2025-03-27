d8ee3636f755e3b88f926916b3bce31854f5fd14
Jack <132464970+jackhfernandez@users.noreply.github.com>
2025-03-26 23:21:11 -0500

    Agregando for, forEach...

--- s01/src/com/example/repeticion/For.java
+++ s01/src/com/example/repeticion/For.java
@@ -2,6 +2,25 @@ package com.example.repeticion;
 
 public class For {
   public static void main(String[] args) {
-    
+
+    for (int i=1; i<3; i++) {
+      System.out.println("El valor de i es " + i);
+    }
+
+    // Recorrer un array
+    String[] nombres = {"Juan", "Franco", "Oscar"};
+
+    for (int i=0; i<nombres.length; i++) {
+      System.out.println(nombres[i]);
+    }
+
+    int[] numeros = {11, 22, 33};
+
+    int suma = 0;
+    for (int i=0; i<numeros.length; i++) {
+      suma += numeros[i];
+    }
+    System.out.println("La suma de elementos es " + suma);
+
   }
 }
--- s01/src/com/example/repeticion/ForEach.java
+++ s01/src/com/example/repeticion/ForEach.java
@@ -3,5 +3,18 @@ package com.example.repeticion;
 public class ForEach {
   public static void main(String[] args) {
 
+    String[] nombres = {"Juan", "Franco", "Oscar"};
+
+    for (String nombre: nombres) {
+      System.out.println(nombre);
+    }
+  
+    int[] numeros = {11, 22, 33};
+
+    int suma = 0;
+    for (int numero: numeros) {
+      suma += numero;
+    }
+    System.out.println("La suma es " + suma);
   }
 }
