ceaf11b5538278a8fcaa1311f98f6c6d5eb38b39
Jack <132464970+jackhfernandez@users.noreply.github.com>
2025-05-16 23:51:58 -0500

    Agregando funciones

---
+++ s06/src/com/example/Funciones.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class Funciones {
+
+  public static void main(String[] args) {
+    int resultado = 0;
+    resultado = suma(4,2);
+    System.out.println("El resultado es " + resultado);
+  }
+
+  public static int suma(int a, int b) {
+    return a + b;
+  }
+}
